64ad2ea406680740edd84fb4a7bdc086c6db6cb8
Eric Cheung <eric1162004@hotmail.com>
2021-05-23 23:58:56 -0700

    Expanded navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,24 +8,28 @@ export default function Navbar(params) {
         <ul className={styles.navbarContactBox}>
           <div className={styles.logo}>
             <img src="/assets/logo1.png" alt="CyberCard Logo" />
-          </div> 
+          </div>
 
-          <Link href="#aboutus">
-            <a className={styles.navbarContact}>About us</a>
-          </Link>
-          <Link href="#contactus">
-            <a className={styles.navbarContact}>Contact us</a>
-          </Link>
-          <Link href="#instructions">
-            <a className={styles.navbarContact}>Instructions</a>
-          </Link>
+          <div className={styles.navbarMiddle}>
+            <Link href="#aboutus">
+              <a className={styles.navbarContact}>About us</a>
+            </Link>
+            <Link href="#contactus">
+              <a className={styles.navbarContact}>Contact us</a>
+            </Link>
+            <Link href="#instructions">
+              <a className={styles.navbarContact}>Instructions</a>
+            </Link>
+          </div>
 
-          <button className={styles.button} href="/">
-            Sign up
-          </button>
-          <button className={styles.button} href="/">
-            Login
-          </button>
+          <div>
+            <button className={styles.button} href="/">
+              Sign up
+            </button>
+            <button className={styles.button} href="/">
+              Login
+            </button>
+          </div>
         </ul>
       </nav>
       <div></div>
