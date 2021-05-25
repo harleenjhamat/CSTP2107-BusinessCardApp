b6bebcf563ffc0b6ffb7977b29131aeadefd29ad
Po-Kai Huang <000429977@student.vcc.ca>
2021-05-25 14:12:13 -0700

    Fix navbar mobile version & getStart font color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { Fragment } from "react";
 import Image from "next/image";
 import styles from "../styles/navbar.module.scss";
@@ -11,10 +10,9 @@ const Navbar = () => {
     <>
       <Fragment>
         
-          <nav className="navbar navbar-expand-lg navbar-light">
-            
+          <nav className={`navbar navbar-expand-lg navbar-light ${styles.navbar}`}>
               <div className="container-fluid">
-              <div className={styles.navbar}>
+
                   <Image
                     className="navbar-brand" 
                     src="/assets/logo2.png"
@@ -22,53 +20,48 @@ const Navbar = () => {
                     height={50}  
                   />
                 
-                <button className="navbar-toggler" 
-                        type="button" 
-                        data-bs-toggle="collapse" 
-                        data-bs-target="#navbarNavDropdown" 
-                        aria-controls="navbarNavDropdown" 
-                        aria-expanded="false" 
-                        aria-label="Toggle navigation" 
-                        >
+                <button 
+                  className="navbar-toggler" 
+                  type="button" 
+                  data-bs-toggle="collapse" 
+                  data-bs-target="#navbarSupportedContent" 
+                  aria-controls="navbarSupportedContent" 
+                  aria-expanded="false" 
+                  aria-label="Toggle navigation" 
+                >
                   <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                  
-                  <ul className="navbar-nav">
-                    <li className="nav-item">
-                      <Link href="#aboutus">
-                        <a className={styles.navbarContact}>About us</a>
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link href="#contactus">
-                        <a className={styles.navbarContact}>Contact us</a>
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link href="#instructions">
-                        <a className={styles.navbarContact}>Instructions</a>
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <button className={styles.button} href="/">
-                        Sign up
-                      </button>
-                    </li>
-                    <li className="nav-item">
-                      <button className={styles.button} href="/">
-                        Login
-                      </button>
-                    </li>
-                  </ul>   
-                </div>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <div className="d-flex flex-grow-1">
+                    <ul className="navbar-nav mb-2 mb-lg-0 flex-grow-1 justify-content-center">
+                      <li className="nav-item">
+                        <a className={` ${styles.navbarContact}`} href="#aboutus">
+                          About
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a className={` ${styles.navbarContact}`} href="#contactus">
+                          Contact
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a className={` ${styles.navbarContact} `} href="#instructions">
+                          Get Started
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                    
+                  <button className={` ${styles.button} `} type="submit">
+                    Login
+                  </button>
+                  <button className={` ${styles.button} `} type="submit">
+                    Sign Up
+                  </button>    
                 </div>
               </div>
-          </nav>
-           
-         
-        
+          </nav>  
       </Fragment>
     </>
   );
