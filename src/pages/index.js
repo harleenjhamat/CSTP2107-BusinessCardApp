89297070cdae23592eaf21b4107c3aead0432512
Eric Cheung <eric1162004@hotmail.com>
2021-05-26 14:01:37 -0700

    imported navbar and footer in app.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,13 @@
-import Navbar from "./../components/Navbar";
 import Card from "../components/Card";
-import AboutUs from "../components/AboutUs";
-import Footer from "../components/Footer";
+import AboutUs from "../components/AboutUs"; 
 import Instructions from "../components/Instruction";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <Card />
       <Instructions/>
       <AboutUs/>
-      <Footer/>
     </div>
   );
 }
