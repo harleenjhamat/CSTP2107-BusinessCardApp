ca0a1fef336ae774469fcd9cdf74d2cd40d56a73
Eric Cheung <eric1162004@hotmail.com>
2021-05-26 15:37:31 -0700

    added mongodb dependency

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -7,15 +7,15 @@ export default NextAuth({
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
+      // authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
     }),
     // ...add more providers here
   ],
 
   // A database is optional, but required to persist accounts in a database
-  // database: process.env.DATABASE_URL,
+  database: process.env.DATABASE_URL, 
 
-  session: {
-    jwt: true,
-  }
+  // session: {
+  //   jwt: true,
+  // }
 });
