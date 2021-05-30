e3094f2d1c592c3452d7bf4784f181140640d2b7
Karla Barrera <000428230@student.vcc.ca>
2021-05-30 02:23:53 -0700

    adds more colour options for card

--- src/pages/custom-card/index.jsx
+++ src/pages/custom-card/index.jsx
@@ -6,30 +6,104 @@ import styles from "../../styles/customcard.module.scss";
 import Image from "next/image";
 
 function CustomCard (props) {
+  const [hoverGrey, setHoverGrey] = useState('');
+  const [hoverOrange, setHoverOrange] = useState('');
   const [hoverGreen, setHoverGreen] = useState('');
   const [hoverBlue, setHoverBlue] = useState('');
   const [hoverRed, setHoverRed] = useState('');
+  const [hoverPurple, setHoverPurple] = useState('');
+  const [hoverPink, setHoverPink] = useState('');
+  const [hoverYellow, setHoverYellow] = useState('');
   const [username, setUsername] = useState('Bob Pancakes');
   const [userphone, setUserphone] = useState('+1(236)333-22-11');
+  var greyHandler = (event) => {
+    setHoverBlue('')
+    setHoverRed('')
+    setHoverPurple('')
+    setHoverYellow('')
+    setHoverGreen('')
+    setHoverOrange('')
+    setHoverPink('')
+    setHoverGrey(`${styles.colorGrey}`);
+  };
   var greenHandler = (event) => {
     setHoverBlue('')
     setHoverRed('')
+    setHoverPurple('')
+    setHoverYellow('')
+    setHoverGrey('')
+    setHoverOrange('')
+    setHoverPink('')
     setHoverGreen(`${styles.colorGreen}`);
+  };
+  var orangeHandler = (event) => {
+    setHoverBlue('')
+    setHoverRed('')
+    setHoverPurple('')
+    setHoverYellow('')
+    setHoverGrey('')
+    setHoverGreen('')
+    setHoverPink('')
+    setHoverOrange(`${styles.colorOrange}`);
   };
   var blueHandler = (event) => {
     setHoverGreen('')
     setHoverRed('')
+    setHoverPurple('')
+    setHoverYellow('')
+    setHoverGrey('')
+    setHoverOrange('')
+    setHoverPink('')
     setHoverBlue(`${styles.colorBlue}`);
   };
   var redHandler = (event) => {
     setHoverGreen('')
     setHoverBlue('')
+    setHoverPurple('')
+    setHoverYellow('')
+    setHoverGrey('')
+    setHoverOrange('')
+    setHoverPink('')
     setHoverRed(`${styles.colorRed}`);
+  };
+  var purpleHandler = (event) => {
+    setHoverGreen('')
+    setHoverBlue('')
+    setHoverRed('')
+    setHoverYellow('')
+    setHoverGrey('')
+    setHoverOrange('')
+    setHoverPink('')
+    setHoverRed(`${styles.colorPurple}`);
+  };
+  var pinkHandler = (event) => {
+    setHoverGreen('')
+    setHoverBlue('')
+    setHoverRed('')
+    setHoverYellow('')
+    setHoverGrey('')
+    setHoverOrange('')
+    setHoverPink(`${styles.colorPink}`);
+  };
+  var yellowHandler = (event) => {
+    setHoverGreen('')
+    setHoverBlue('')
+    setHoverRed('')
+    setHoverPurple('')
+    setHoverGrey('')
+    setHoverOrange('')
+    setHoverPink('')
+    setHoverRed(`${styles.colorYellow}`);
   };
   var resetColorHandler = (event)=>{
     setHoverGreen('')
     setHoverBlue('')
-    setHoverRed('');
+    setHoverRed('')
+    setHoverGrey('')
+    setHoverYellow('')
+    setHoverOrange('')
+    setHoverPink('')
+    setHoverPurple('');
   }
   var nameHandler = (event)=>{
     setUsername(event.target.value);
@@ -40,7 +114,7 @@ function CustomCard (props) {
   return (
     <Fragment>
       <Head>
-        <title>Custom Card</title>
+        <title> Your Custom Card</title>
         <meta name='description' content='Customize your Card' />
       </Head>
       <Navbar />
@@ -53,21 +127,25 @@ function CustomCard (props) {
             </h1>
           </div>
         </div>
+        <div>
+          <input type="file" id="actual-btn" hidden/>
+          <label className={`${styles.imageUser}`} htmlFor="actual-btn">Upload Photo</label>
+        </div>
         {/* BORDER */}
         <div className={`row justify-content-center my-2 mx-2`}>
-          <div className={`col-12 col-md-10 col-lg-5 my-2 ${styles.cardBorder} ${styles.cen} ${hoverGreen} ${hoverBlue} ${hoverRed}`}>
+          <div className={`col-12 col-md-10 col-lg-5 my-2 ${styles.cardBorder} ${styles.cen} ${hoverGrey} ${hoverGreen} ${hoverOrange} ${hoverPink} ${hoverBlue} ${hoverRed} ${hoverPurple} ${hoverYellow}`}>
             {/* INNER PART */}
             <div className="row justify-content-center">
               <div className={`col-7 my-4 ${styles.left}`}>
                 <Image className={`border border-info border-5 rounded-3`} src="/assets/business_logo.png" alt="instruction1" width={200} height={70}/>
-                <h2 className={`mt-3`}>Business name</h2>
-                <h4 className={`mt-4`}>{username}</h4>
-                <p>{userphone}</p>
+                <h2 className={`mt-3 ${styles.cardFont}`}>Business name</h2>
+                <h4 className={`mt-4 ${styles.cardFont}`}>{username}</h4>
+                <p className={`${styles.cardFont}`}>{userphone}</p>
               </div>
               <div className={`col-4 my-4 ${styles.cen}`}>
                 <Image className={`border border-info border-5 ${styles.roundElem}`} src="/assets/img_avatar.png" alt="instruction1" width={200} height={200}/>
-                <p className={`p-0 m-0 ${styles.cen}`}>my@email.com</p>
-                <p className={`p-0 m-0 ${styles.cen}`}>Powered by Cybercard</p>
+                <p className={`p-0 m-0 ${styles.cen} ${styles.cardFont} `}>my@email.com</p>
+                <p className={`p-0 m-0 ${styles.cen} ${styles.cardFont} `}>Powered by Cybercard</p>
               </div>
             </div>
           </div>
@@ -88,6 +166,18 @@ function CustomCard (props) {
               <div className={`col-6 mt-4 p-0 ${styles.cen}`}>
                 <div className={`row justify-content-center m-0 p-0`}>
                   {/* 3 cols for colors: */}
+                  <div className={`col-1 m-0 p-0 ${styles.cen}`}  onClick={greyHandler}>
+                    <h3 className={`${styles.colorGrey}`}>g</h3>
+                  </div>
+                  <div className={`col-1 m-0 p-0 ${styles.cen}`}>
+                    <h3 className={`${styles.colorWhite}`}>&#32;</h3>
+                  </div>
+                  <div className={`col-1 m-0 p-0 ${styles.cen}`} onClick={yellowHandler}>
+                    <h3 className={`${styles.colorYellow}`}>Y</h3>
+                  </div>
+                  <div className={`col-1 m-0 p-0 ${styles.cen}`}>
+                    <h3 className={`${styles.colorWhite}`}>&#32;</h3>
+                  </div>
                   <div className={`col-1 m-0 p-0 ${styles.cen}`}  onClick={greenHandler}>
                     <h3 className={`${styles.colorGreen}`}>G</h3>
                   </div>
@@ -100,9 +190,31 @@ function CustomCard (props) {
                   <div className={`col-1 m-0 p-0 ${styles.cen}`}>
                     <h3 className={`${styles.colorWhite}`}>&#32;</h3>
                   </div>
+                  <div className={`col-1 m-0 p-0 ${styles.cen}`} onClick={purpleHandler}>
+                    <h3 className={`${styles.colorPurple}`}>P</h3>
+                  </div>
+                  <div className={`col-1 m-0 p-0 ${styles.cen}`}>
+                    <h3 className={`${styles.colorWhite}`}>&#32;</h3>
+                  </div>
                   <div className={`col-1 m-0 p-0 ${styles.cen}`} onClick={redHandler}>
                     <h3 className={`${styles.colorRed}`}>R</h3>
                   </div>
+                  <div className={`col-1 m-0 p-0 ${styles.cen}`}>
+                    <h3 className={`${styles.colorWhite}`}>&#32;</h3>
+                  </div>
+                  <div className={`col-1 m-0 p-0 ${styles.cen}`}  onClick={orangeHandler}>
+                    <h3 className={`${styles.colorOrange}`}>O</h3>
+                  </div>
+                  <div className={`col-1 m-0 p-0 ${styles.cen}`}>
+                    <h3 className={`${styles.colorWhite}`}>&#32;</h3>
+                  </div>
+                  <div className={`col-1 m-0 p-0 ${styles.cen}`}  onClick={pinkHandler}>
+                    <h3 className={`${styles.colorPink}`}>P</h3>
+                  </div>
+                  <div className={`col-1 m-0 p-0 ${styles.cen}`}>
+                    <h3 className={`${styles.colorWhite}`}>&#32;</h3>
+                  </div>
+                  
                 </div>
               </div>
             </div>
