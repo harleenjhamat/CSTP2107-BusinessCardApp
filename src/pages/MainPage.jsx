243e390de5d222b48e13d8c4b7245b1fd69e0312
Po-Kai Huang <000429977@student.vcc.ca>
2021-06-05 12:46:03 -0700

    Added files

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -22,16 +22,17 @@ const MainPage = () => (
                     </button>
                 </span>
             </div>
+            <br></br>
             <div className="accordion" id="accordionExample">
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingOne">
                     <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        Group 1
+                        Group 1 <img />
                     </button>
                     </h2>
                     <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                     <div className="accordion-body">
-                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        
                     </div>
                     </div>
                 </div>
@@ -60,9 +61,10 @@ const MainPage = () => (
                     </div>
                 </div>
                 
-                <button className={styles.button}> + + Add New Group + + </button>
+                
             </div>
         </div>
+        <button className={styles.button}> + + Add New Group + + </button>
         
     </Fragment>
 )
