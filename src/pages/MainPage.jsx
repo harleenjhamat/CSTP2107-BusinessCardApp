cc893c255ddd9c250edfceca3c51dc15c5b5181b
Po-Kai Huang <000429977@student.vcc.ca>
2021-06-05 09:35:33 -0700

    Added mainpage files

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Fragment } from 'react';
+import { Button, Card, CardDescription, Image, Icon } from 'semantic-ui-react'
+import styles from "../styles/mainpage.module.scss";
+import Head from 'next/head'
+
+const MainPage = () => (
+
+    <Fragment>
+      <Head>
+        <title>HomePage</title>
+      </Head>
+        
+    </Fragment>
+)
+
+
+export default MainPage
