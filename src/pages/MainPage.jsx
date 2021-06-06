2bfe0da9ec5e857f63f15a6d84efc6e8774fc945
Po-Kai Huang <000429977@student.vcc.ca>
2021-06-06 12:01:34 -0700

    Add cards in each groups & buttons

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -26,30 +26,49 @@ const MainPage = () => (
             </div>
             <br></br>
 
-            {/*Dropdown to see Cards*/}
+            {/*Dropdown */}
             <div className="accordion" id="accordionExample">
+                
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingOne">
-                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        My Own Card  
+                        <button className={`accordion-button ${styles.dropdownContainer}`} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            My Own Card  
                             <div >
-                                <a className={ styles.dropdownContainer } href="http://localhost:3000/TemplatePage">
+                                <a href="/">
+                                    <Image 
+                                        src="/assets/editGroupName.png"
+                                        alt="Edit Group Name"
+                                        width={30}
+                                        height={30} 
+                                    />
+                                </a>
+                                <a href="http://localhost:3000/TemplatePage">
                                     <Image 
                                         src="/assets/add-card.png"
-                                        alt="add icon"
+                                        alt="Create Card"
                                         width={40}
-                                        height={40}
-                                        className={ styles.AddCardButton }
+                                        height={40}         
                                     />
                                 </a>
                             </div>
                         </button>
                     </h2>
-                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                    <div className="accordion-body">
 
                     {/*Display user's own card*/}
-
+                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div className="accordion-body">                   
+                    <div className={ styles.CardDisplay }>
+                        <a href="http://localhost:3000/custom-card">
+                        <Image
+                            src="/assets/card1.png"
+                            alt="Create Card"
+                            width={500}
+                            height={300}
+                            className={ styles.CardDisplay }
+                            
+                        />
+                        </a>
+                    </div>
 
                     {/*Share & Delete Card*/}
                     <div className={styles.buttonContainer}>
@@ -65,10 +84,9 @@ const MainPage = () => (
                         <button className={styles.MyOwnCardButtonDelete}>
                             <Image
                                 src="/assets/deleteCard.png"
-                                alt="share icon"
+                                alt="Delete icon"
                                 width={30}
                                 height={30}
-                                quality="high"
                             />
                         </button>
                     </div>
@@ -79,13 +97,63 @@ const MainPage = () => (
 
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingTwo">
-                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    <button className={`accordion-button collapsed ${styles.dropdownContainer}`} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                         Group 1
+                        <div >
+                            <a href="/">
+                                <Image 
+                                    src="/assets/editGroupName.png"
+                                    alt="Edit Group Name"
+                                    width={30}
+                                    height={30} 
+                                    />
+                            </a>
+                            <a href="http://localhost:3000/TemplatePage">
+                                <Image 
+                                    src="/assets/add-card.png"
+                                    alt="Create Card"
+                                    width={40}
+                                    height={40}         
+                                />
+                            </a>
+                        </div>
                     </button>
                     </h2>
                     <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                     <div className="accordion-body">
-                        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                    <div className={ styles.CardDisplay }>
+                        <Image
+                            src="/assets/card1.png"
+                            alt="Create Card"
+                            width={500}
+                            height={300}
+                            className={ styles.CardDisplay }
+                        />
+                    </div>
+
+                    {/*Share & Delete Card*/}
+                    <div >
+
+                        <button className={styles.GroupButtonDelete}>
+                            Delete Card<Image
+                                src="/assets/deleteCard.png"
+                                alt="Delete icon"
+                                width={30}
+                                height={30}
+                            />
+                        </button>
+
+                        <button className={styles.GroupButtonDelete}>
+                            Delete Group<Image
+                                src="/assets/deleteCard.png"
+                                alt="Delete icon"
+                                width={30}
+                                height={30}
+                            />
+                        </button>
+
+                    </div>
+                    
                     </div>
                     </div>
                 </div>
@@ -93,13 +161,63 @@ const MainPage = () => (
 
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingThree">
-                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    <button className={`accordion-button collapsed ${styles.dropdownContainer}`} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                         Group 2
+                        <div >
+                            <a href="/">
+                                <Image 
+                                    src="/assets/editGroupName.png"
+                                    alt="Edit Group Name"
+                                    width={30}
+                                    height={30} 
+                                    />
+                            </a>
+                            <a href="http://localhost:3000/TemplatePage">
+                                <Image 
+                                    src="/assets/add-card.png"
+                                    alt="Create Card"
+                                    width={40}
+                                    height={40}         
+                                />
+                            </a>
+                        </div>
                     </button>
                     </h2>
                     <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                     <div className="accordion-body">
-                        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                    <div className={ styles.CardDisplay }>
+                        <Image
+                            src="/assets/card1.png"
+                            alt="Create Card"
+                            width={500}
+                            height={300}
+                            className={ styles.CardDisplay }
+                        />
+                    </div>
+
+                    {/*Share & Delete Card*/}
+                    <div >
+
+                        <button className={styles.GroupButtonDelete}>
+                            Delete Card<Image
+                                src="/assets/deleteCard.png"
+                                alt="Delete icon"
+                                width={30}
+                                height={30}
+                            />
+                        </button>
+
+                        <button className={styles.GroupButtonDelete}>
+                            Delete Group<Image
+                                src="/assets/deleteCard.png"
+                                alt="Delete icon"
+                                width={30}
+                                height={30}
+                            />
+                        </button>
+
+                    </div>
+                        
                     </div>
                     </div>
                 </div>
@@ -112,7 +230,7 @@ const MainPage = () => (
         <div className={ styles.container }>
             <button className={styles.button}> + + Add New Group + + </button>
         </div>
-        
+
     </Fragment>
 )
 
