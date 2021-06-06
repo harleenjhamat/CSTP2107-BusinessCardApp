5cc1ab68dbae5a7cff0b0562c35f1962a7a35a9e
Po-Kai Huang <000429977@student.vcc.ca>
2021-06-06 10:39:10 -0700

    Added share & delete button in my own card

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -7,6 +7,8 @@ const MainPage = () => (
 
     <Fragment>
         <br></br>
+        { /*Search Cards*/ }
+
         <div className={ styles.container }>
             <div className="input-group rounded">
                 <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search"
@@ -23,23 +25,62 @@ const MainPage = () => (
                 </span>
             </div>
             <br></br>
+
+            {/*Dropdown to see Cards*/}
             <div className="accordion" id="accordionExample">
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingOne">
-                    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        Group 1 <img />
-                    </button>
+                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        My Own Card  
+                            <div >
+                                <a className={ styles.dropdownContainer } href="http://localhost:3000/TemplatePage">
+                                    <Image 
+                                        src="/assets/add-card.png"
+                                        alt="add icon"
+                                        width={40}
+                                        height={40}
+                                        className={ styles.AddCardButton }
+                                    />
+                                </a>
+                            </div>
+                        </button>
                     </h2>
                     <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                     <div className="accordion-body">
+
+                    {/*Display user's own card*/}
+
+
+                    {/*Share & Delete Card*/}
+                    <div className={styles.buttonContainer}>
+                        <button className={styles.MyOwnCardButtonShare}> 
+                            <Image
+                                src="/assets/share.png"
+                                alt="share icon"
+                                width={30}
+                                height={30}
+                            />
+                        </button>
                         
+                        <button className={styles.MyOwnCardButtonDelete}>
+                            <Image
+                                src="/assets/deleteCard.png"
+                                alt="share icon"
+                                width={30}
+                                height={30}
+                                quality="high"
+                            />
+                        </button>
+                    </div>
                     </div>
                     </div>
                 </div>
+
+
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingTwo">
                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        Group 2
+                        Group 1
                     </button>
                     </h2>
                     <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
@@ -48,10 +89,12 @@ const MainPage = () => (
                     </div>
                     </div>
                 </div>
+
+
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingThree">
                     <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        Group 3
+                        Group 2
                     </button>
                     </h2>
                     <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
@@ -64,7 +107,11 @@ const MainPage = () => (
                 
             </div>
         </div>
-        <button className={styles.button}> + + Add New Group + + </button>
+
+        {/* Add New Group */}
+        <div className={ styles.container }>
+            <button className={styles.button}> + + Add New Group + + </button>
+        </div>
         
     </Fragment>
 )
