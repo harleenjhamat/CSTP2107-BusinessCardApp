6c756531279c80a902974995d61eecd8bfb4740a
Ian McConaghy <000436891@student.vcc.ca>
2021-06-20 12:51:22 -0700

    Chaging columns, button style, sizing etc

--- src/components/SearchCard.jsx
+++ src/components/SearchCard.jsx
@@ -1,6 +1,7 @@
 import { useRef } from 'react';
 import { Fragment } from 'react';
 import styles from "@/styles/sharedcard.module.scss";
+import { Container } from '@material-ui/core';
 
 var row_center = `row m-0 p-0 justify-content-center`
 var row_default = `row m-0 p-0`
@@ -8,44 +9,42 @@ var col = `text-center m-0 p-0`
 
 const SearchCard = (props) => {
     const sharedcode = useRef('');
-    const GetCardByCode = () =>{
+    const GetCardByCode = () => {
         console.log(sharedcode)
         const sendObject = {
             addcard: sharedcode.current.value
             // addcard: "0.39290873404547777"
-          }
-          const sendObjectStr = JSON.stringify(sendObject)
+        }
+        const sendObjectStr = JSON.stringify(sendObject)
         fetch("http://localhost:3000/api/usercards/?=", {
-        "method": "POST",
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": sendObjectStr
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": sendObjectStr
         })
-        .then(function (response) {
-            return response.json()
-          })
-          .then(function (data) {
-            // console.log(data[data.length - 1].img)
-            props.addnewcard(data[data.length - 1].img)
-          })
-        
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                // console.log(data[data.length - 1].img)
+                props.addnewcard(data[data.length - 1].img)
+            })
+
     }
-    return(
-    <Fragment>
-    <div className={`${row_center}`}>
-        <div className={`${col} col-12`}>
-            <div className={`${row_default} justify-content my-4`}>
-                <div className={`${col} col-12 col-md-8 mb-2`}>
-                    <input type="text" placeholder="search card here.." className={`form-control`} ref={sharedcode}/>
+    return (
+        <Fragment>
+
+            <div className={`${row_center}`}>
+                <div className={`${col} col-4`}>
+                    <input type="text" placeholder="search card here.." className={`form-control`} ref={sharedcode} />
                 </div>
-                <div className={`${col} col-12 col-md-3 mb-2 d-grid`}>
+                <div className={`${col} col-12 col-md-3 mb-2`}>
                     <button className={`${styles.button}`} onClick={GetCardByCode}>Search</button>
                 </div>
             </div>
-        </div>
-    </div>
-    </Fragment>)
+            <br></br>
+        </Fragment>)
 }
 
 export default SearchCard
