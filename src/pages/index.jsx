bb611a9917375acbbdb52912d55c584f8eddff9f
Eric Cheung <eric1162004@hotmail.com>
2021-06-30 13:55:39 -0700

    call for action

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,23 @@
+import { useRouter } from "next/router";
+
 import Card from "../components/Card";
 import Instructions from "../components/Instruction";
+import styles from "../styles/landingPage.module.scss";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleGetStart = () => {
+    router.push("/custom-card");
+  };
+
   return (
-    <div>
+    <div className={styles.container}>
       <Card />
       <Instructions />
+      <button className={styles.callForActionBtn} onClick={handleGetStart}>
+        Start Now
+      </button>
     </div>
   );
 }
