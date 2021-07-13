e29b73bc95d133c41f276f02ea983bbe8d3d3620
Karla Barrera <000428230@student.vcc.ca>
2021-07-13 16:03:43 -0700

    made card in banner opaque

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,8 +12,8 @@ const Card = () => {
     <>
       <div className={styles.container}>
       <div className={styles.banner}>
-        <p>You Had My Card Hello!</p>
-        <h1>Design and Share Your Business Card.</h1>
+        <p>You Had My at Card Hello!</p>
+        <h1>Design and Share Your Business Card</h1>
       </div>
         <button
           className={`btn btn-lg fw-bold ${styles.getStartBtn}`}
