74036b2078d87632976b5a7b717d9cae8f1ed4ae
Karla Barrera <000428230@student.vcc.ca>
2021-07-12 21:55:44 -0700

    updated image sizes and instruction content

--- src/components/Instruction.jsx
+++ src/components/Instruction.jsx
@@ -12,38 +12,35 @@ const Instructions = () => {
                   src="/assets/choose_template.jpg"
                   alt="instruction1"
                   width={600}
-                  height={380}
+                  height={300}
                 />
             </div>
             <div className={`col-12 col-md-6 order-md-1 ${styles.card}`}>
-              <h1>Choose a template</h1>
+              <h1>Choose a Template</h1>
               <p>
-                Start by selecting a template for your card. We have a wide
-                range of templates that come in different colours, designs and
-                business logos. If you do not like any template, no worries! You
-                can customize your card by selecting a blank template.
+                Start by selecting a template: we have a wide range of templates in different colours, designs and
+                business logos. Don't see one you like? You can start with a blank canvas and build from there! 
               </p>
             </div>
           </div>
         </div>
 
         <div className="my-4">
-          <div className="row align-items-center justify-content-around">
-            <div className="col-12 col-md-5 p-1">
+          <div className="row align-items-center justify-content-around pt-4">
+            <div className="col-6 col-md-4">
               <Image
                 src="/assets/choose_template.jpg"
                 alt="instruction2"
                 width={600}
-                height={380}
+                height={370.467}
+                
               />
             </div>
             <div className={`col-12 col-md-6 order-md-1 ${styles.card}`}>
-              <h1>Customize your card</h1>
+              <h1>Customize your Card</h1>
               <p>
-                At this stage, you can make customizations to your card, Like
-                changing the colour, font and size of the text or adding another
-                text field. Along with that, you can also have an image on your
-                card which makes it look more professional.
+                Now it's time to customize your card. You can change the colour, font and size of the text, or add another
+                text field. You can even add images to give it a professional edge. You're in control.
               </p>
             </div>
           </div>
@@ -51,22 +48,20 @@ const Instructions = () => {
 
         <div className="py-4">
           <div className="row align-items-center justify-content-around">
-            <div className="col-12 col-md-5 order-md-2 p-1">
+            <div className="col-12 col-md-5 order-md-2">
               <Image
                 src="/assets/share_your_card.jpg"
                 alt="instruction3"
                 width={600}
-                height={380}
+                height={300}
               />
             </div>
             <div className={`col-12 col-md-6 order-md-1 ${styles.card}`}>
-              <h1>Save and share</h1>
+              <h1>Save and Share</h1>
               <p>
-                After making final changes to your card, you can easily save
-                your card to your device. Moreover, we are providing you with
-                another option too, i.e, directly share it with your friends,
-                family or colleagues. See, how fun and easy it is to make a
-                card!
+                After adding those final touches, you can easily save
+                your card to your device. Or take advantage of our sharing feature and instantly share it with your friends,
+                family, and colleagues! 
               </p>
             </div>
           </div>
