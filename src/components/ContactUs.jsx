bc9436190e851973dd27efc9c4b0eeb72caafe7a
Ian McConaghy <000436891@student.vcc.ca>
2021-07-12 23:06:58 -0700

    Modifying text styling

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -7,7 +7,7 @@ const ContactUs = () => {
     <>    
     <div className={styles.container}>
         <h1>Contact Us</h1>
-        <p>Want to get in touch? Send us a message and we will be in touch within one business day</p>
+        <p><b>Want to get in touch? Send us a message and we will get back to you within one business day</b></p>
     </div>
     <div className={styles.row}>
 
@@ -15,30 +15,30 @@ const ContactUs = () => {
             <div className={styles.contactcol}>
                 <i className='fa fa-home'></i>
                 <span>
-                    <h5>XYZ Road, ABC Building</h5>
-                    <p>Surrey, BC</p>
+                    <h5><b>VCC Downtown, 250 W Pender St</b></h5>
+                    <p><b>Vancouver, BC</b></p>
                 </span>
             </div>
             <div className={styles.contactcol}>
                 <i className='fa fa-phone'></i>
                 <span>
-                    <h5>+1(778)-100-1000</h5>
-                    <p>Monday - Saturday, 10AM to 6PM</p>
+                    <h5><b>+1 (604) 871-7000</b></h5>
+                    <p><b>Monday - Friday, 9:30AM to 4PM</b></p>
                 </span>
             </div>
 
             <div className={styles.contactcol}>
                 <i className='fa fa-envelope-o'></i>
                 <span>
-                    <h5>card@cybercard.com</h5>
-                    <p>Email us your query</p>
+                    <h5><b>card@cybercard.com</b></h5>
+                    <p><b>Email us your inquery</b></p>
                 </span>
             </div>
         </div>
         <div className={styles.contactcol}>
             <form action=''>
-            <h1>Questions? </h1>
-            <p>Please drop your message below and we will be in touch with you shortly</p>
+            <h1>Questions?</h1>
+            <p><b>Please leave your message below and we will be in touch with you shortly</b></p>
                 <input type='text' placeholder='Enter your name' required />
                 <input type='email' placeholder='Enter email address' required />
                 <input type='text' placeholder='Enter your subject' required />
