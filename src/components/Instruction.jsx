6236f030645757ce41a4e18ccff7128e6c704ed7
Karla Barrera <000428230@student.vcc.ca>
2021-07-18 19:11:29 -0700

    updated first card's content on lading page

--- src/components/Instruction.jsx
+++ src/components/Instruction.jsx
@@ -16,27 +16,26 @@ const Instructions = () => {
                 />
             </div>
             <div className={`col-12 col-md-6 order-md-1 ${styles.card}`}>
-              <h1>Choose a Template</h1>
+              <h1>Use Anytime, Anywhere</h1>
               <p>
-                Start by selecting a template: we have a wide range of templates in different colours, designs and
-                business logos. Don't see one you like? You can start with a blank canvas and build from there! 
+                No more lost, outdated, or bent businesscards. All you need is your desktop of mobile device, and you're all set to build your web of professional contacts!
+                
               </p>
             </div>
           </div>
         </div>
 
-        <div className="my-4">
-          <div className="row align-items-center justify-content-around pt-4">
-            <div className="col-6 col-md-4">
+        <div className="py-4">
+          <div className="row align-items-center justify-content-center">
+            <div className="col-12 col-md-5 order-md-1 p-1">
               <Image
                 src="/assets/bot_custom.jpg"
                 alt="instruction2"
-                width={600}
-                height={370.467}
-                
+                width={550}
+                height={305}
               />
             </div>
-            <div className={`col-12 col-md-6 order-md-1 ${styles.card}`}>
+            <div className={`col-12 col-md-6 order-md-2 ${styles.card}`}>
               <h1>Customize your Card</h1>
               <p>
                 Now it's time to customize your card. You can change the colour, font and size of the text, or add another
@@ -48,7 +47,7 @@ const Instructions = () => {
 
         <div className="py-4">
           <div className="row align-items-center justify-content-around">
-            <div className="col-12 col-md-5 order-md-2">
+            <div className="col-12 col-md-5 order-md-2 p-1">
               <Image
                 src="/assets/bot_share.jpg"
                 alt="instruction3"
@@ -66,6 +65,10 @@ const Instructions = () => {
             </div>
           </div>
         </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
       </div>
     </>
   );
