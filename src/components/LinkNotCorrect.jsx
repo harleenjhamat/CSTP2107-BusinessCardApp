ff029a7aa0b8c2c53f09f457479e7fe001f089da
Mikhail Kashkov <mikhail.kashkov@student.vcc.ca>
2021-08-01 18:14:06 -0700

    Better rendering Links +add card feature +comments

---
+++ src/components/LinkNotCorrect.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from 'react'
+const LinkNotCorrect = () => (
+  <Fragment>
+    <div className='row justify-content-center mt-4'>
+      <div className='col-12 col-md-6 text-center mt-4'>
+        <div className='alert alert-danger'>
+          <h1>Link is not correct</h1>
+        </div>
+      </div>
+    </div>
+  </Fragment>
+)
+
+export default LinkNotCorrect
